99f671b95ee83cac480f604874819ac5fd31f21a
Atchyut Nagabhairava <atchyutnagabhairava@gmail.com>
2022-04-22 19:05:01 +0530

    Initial Commit

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+
+$keyId = 'rzp_test_XXXXXXXXXXXX';
+$keySecret = 'XXXXXXXXXXXXX';
+$displayCurrency = 'INR';
+
+//These should be commented out in production
+// This is for error reporting
+// Add it to config.php to report any errors
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+
+//DATABASE CONNECTION DETAILS
+$host = "localhost";
+$username = "u223156778_root";
+$password = "Password*1";
+$dbname = "u223156778_shop";
---
+++ default.php
@@ -0,0 +1,164 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Payment Form</title>
+</head>
+<body>
+<!--    <form action="pay.php" method="post">-->
+<!--        Price: <input type="text" name="price"><br>-->
+<!--        Name: <input type="text" name="customername"><br>-->
+<!--        Email: <input type="email" name="email"><br>-->
+<!--        Contact No: <input type="text" name="contactno"><br>-->
+<!--        <input type="submit" name="submit" value="Proceed To Pay">-->
+<!--</form>-->
+
+<div class="container">  
+  <form id="contact" action="pay.php" method="post">
+    <h3>Prototype for payment</h3>
+    <h4>Fill the form and make payment!</h4>
+    <fieldset>
+      <input placeholder="Amount to be paid" type="number" tabindex="1" name="price" required autofocus>
+    </fieldset>
+    <fieldset>
+      <input placeholder="Your name" type="text" tabindex="1" name="customername" required autofocus>
+    </fieldset>
+    <fieldset>
+      <input placeholder="Your Email Address" type="email" tabindex="2" name="email" required>
+    </fieldset>
+    <fieldset>
+      <input placeholder="Your Phone Number" type="number" tabindex="3" name="contactno" required>
+    </fieldset>
+    <fieldset>
+      <!--<button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Submit</button>-->
+      <button type="submit" name="submit" id="contact-submit">Proceed to pay</button>
+    </fieldset>
+  </form>
+ 
+  
+</div>
+</body>
+
+
+<style>
+    @import url(https://fonts.googleapis.com/css?family=Open+Sans:400italic,400,300,600);
+
+* {
+	margin:0;
+	padding:0;
+	box-sizing:border-box;
+	-webkit-box-sizing:border-box;
+	-moz-box-sizing:border-box;
+	-webkit-font-smoothing:antialiased;
+	-moz-font-smoothing:antialiased;
+	-o-font-smoothing:antialiased;
+	font-smoothing:antialiased;
+	text-rendering:optimizeLegibility;
+}
+
+body {
+	font-family:"Open Sans", Helvetica, Arial, sans-serif;
+	font-weight:300;
+	font-size: 12px;
+	line-height:30px;
+	color:#777;
+	background:#F98B36;
+}
+
+.container {
+	max-width:400px;
+	width:100%;
+	margin:0 auto;
+	position:relative;
+}
+
+#contact input[type="text"], #contact input[type="email"], #contact input[type="tel"], #contact input[type="url"], #contact textarea, #contact button[type="submit"] { font:400 12px/16px "Open Sans", Helvetica, Arial, sans-serif; }
+
+#contact {
+	background:#F9F9F9;
+	padding:25px;
+	margin:50px 0;
+}
+
+#contact h3 {
+	color: #F96;
+	display: block;
+	font-size: 30px;
+	font-weight: 400;
+}
+
+#contact h4 {
+	margin:5px 0 15px;
+	display:block;
+	font-size:13px;
+}
+
+fieldset {
+	border: medium none !important;
+	margin: 0 0 10px;
+	min-width: 100%;
+	padding: 0;
+	width: 100%;
+}
+
+#contact input[type="text"], #contact input[type="email"], #contact input[type="tel"],#contact input[type="number"], #contact input[type="url"], #contact textarea {
+	width:100%;
+	border:1px solid #CCC;
+	background:#FFF;
+	margin:0 0 5px;
+	padding:10px;
+}
+
+#contact input[type="text"]:hover, #contact input[type="email"]:hover, #contact input[type="tel"]:hover, #contact input[type="url"]:hover, #contact textarea:hover {
+	-webkit-transition:border-color 0.3s ease-in-out;
+	-moz-transition:border-color 0.3s ease-in-out;
+	transition:border-color 0.3s ease-in-out;
+	border:1px solid #AAA;
+}
+
+#contact textarea {
+	height:100px;
+	max-width:100%;
+  resize:none;
+}
+
+#contact button[type="submit"] {
+	cursor:pointer;
+	width:100%;
+	border:none;
+	background:#0CF;
+	color:#FFF;
+	margin:0 0 5px;
+	padding:10px;
+	font-size:15px;
+}
+
+#contact button[type="submit"]:hover {
+	background:#09C;
+	-webkit-transition:background 0.3s ease-in-out;
+	-moz-transition:background 0.3s ease-in-out;
+	transition:background-color 0.3s ease-in-out;
+}
+
+#contact button[type="submit"]:active { box-shadow:inset 0 1px 3px rgba(0, 0, 0, 0.5); }
+
+#contact input:focus, #contact textarea:focus {
+	outline:0;
+	border:1px solid #999;
+}
+::-webkit-input-placeholder {
+ color:#888;
+}
+:-moz-placeholder {
+ color:#888;
+}
+::-moz-placeholder {
+ color:#888;
+}
+:-ms-input-placeholder {
+ color:#888;
+}
+
+</style>
+</html>
